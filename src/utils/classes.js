b1c11d41fcb5daf9d0eb3b0ebf7781efab1b231c
Bartek <b.pyzocha@gmail.com>
2022-05-07 20:32:42 +0200

    Return undefind if no class provided

--- src/utils/classes.js
+++ src/utils/classes.js
@@ -1,8 +1,10 @@
 const classes = (...arr) => {
-  return arr
-    .filter((cls) => !!cls)
-    .map((cls) => cls.trim())
-    .join(' ');
+  return (
+    arr
+      .filter((cls) => !!cls)
+      .map((cls) => cls.trim())
+      .join(' ') || undefined
+  );
 };
 
 export default classes;
