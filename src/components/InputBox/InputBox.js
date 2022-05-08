021bcc31b74fb78a732fc6e9adfb6d5f432b25de
Bartek <b.pyzocha@gmail.com>
2022-05-08 21:09:04 +0200

    Refactor code

--- src/components/InputBox/InputBox.js
+++ src/components/InputBox/InputBox.js
@@ -2,20 +2,25 @@ import classes from '../../utils/classes';
 import cls from './InputBox.module.css';
 
 const InputBox = ({ label, errors, labelFor, children, className }) => {
+  const labelElem = label && (
+    <label htmlFor={labelFor} className={cls.label}>
+      {label}
+    </label>
+  );
+
+  const errorElems =
+    errors &&
+    errors.map((error) => (
+      <span key={error} className={cls.error}>
+        {error}
+      </span>
+    ));
+
   return (
     <div className={classes(className, cls.box)}>
-      {label && (
-        <label htmlFor={labelFor} className={cls.label}>
-          {label}
-        </label>
-      )}
+      {labelElem}
       {children}
-      {errors &&
-        errors.map((error) => (
-          <span key={error} className={cls.error}>
-            {error}
-          </span>
-        ))}
+      {errorElems}
     </div>
   );
 };
